f27e2da143aaa47dd2bcdc54b23be95a063ccbd3
itsmanjeet <itsmanjeet1998@gmail.com>
2021-11-15 15:43:33 +0530

    fixed path

--- scripts/genui.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-_UI="$(cat ${1})"
-
-echo "package app
-
-const UI = \`${_UI}\`" > app/ui.go
-
--- scripts/install.sh
+++ scripts/install.sh
@@ -4,6 +4,6 @@ DESTDIR=${DESTDIR:-"/"}
 VERSION=${VERSION:-'2110'}
 
 install -v -d -m 0755 cmd ${DESTDIR}/usr/bin/
-go build -o ${DESTDIR}/usr/bin/sys-setup cmd/main.go
+go build -o ${DESTDIR}/usr/bin/sys-setup-usr cmd/main.go
 
 install -v -D -m 0644 perm/dev.rlxos.system-setup.policy -t ${DESTDIR}/usr/share/polkit-1/actions/

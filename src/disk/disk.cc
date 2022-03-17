96e2df00db69230954ca76c1adb45ad0e52273dd
itsmanjeet <itsmanjeet1998@gmail.com>
2022-03-17 11:49:01 +0530

    [FIXED]: Crash on missing part uuid path

--- src/disk/disk.cc
+++ src/disk/disk.cc
@@ -4,7 +4,7 @@
 #include <iostream>
 
 #include "../utils/exec.hh"
-
+#define DISK_PART_UUID_PATH "/dev/disk/by-partuuid"
 namespace fs = std::filesystem;
 
 Disk::Disk(std::string const& path) : m_Path(path) {
@@ -15,10 +15,20 @@ Disk::Disk(std::string const& path) : m_Path(path) {
 
 std::vector<Disk> Disk::list() {
   std::vector<Disk> disks;
-  fs::current_path("/dev/disk/by-partuuid");
-  for (auto const& i : fs::directory_iterator(".")) {
-    auto disk_path = fs::canonical(fs::absolute(fs::read_symlink(i.path())));
-    disks.push_back(Disk(disk_path.string()));
+  if (!std::filesystem::exists(DISK_PART_UUID_PATH)) {
+    system("gparted");
+  }
+
+  try {
+    fs::current_path(DISK_PART_UUID_PATH);
+    for (auto const& i : fs::directory_iterator(".")) {
+      auto disk_path = fs::canonical(fs::absolute(fs::read_symlink(i.path())));
+      disks.push_back(Disk(disk_path.string()));
+    }
+  } catch (std::exception const& exc) {
+    std::cerr << "Failed to read disk paritition, no " DISK_PART_UUID_PATH
+                 " exists"
+              << std::endl;
   }
 
   return disks;
